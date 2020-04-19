74815edce40b11fad8c8db8e121539c7b4630bfa
Segun Adebayo <joseshegs@gmail.com>
2020-04-20 01:29:44 +0400

    feat: add deep copy

--- src/toolkit.ts
+++ src/toolkit.ts
@@ -6,36 +6,61 @@ import deleter from "unset-value";
 import isEqual from "dequal";
 import renamer from "deep-rename-keys";
 import { Dict, resolvePath } from "./utils";
+import copy from "deep-copy";
 
-export type RenameKeysIterator = (key: string) => string;
+export type RenameKeysFn = (key: string) => string;
 
-export function renameKeys<T extends Dict>(object: T, fn: RenameKeysIterator) {
+/**
+ * Recursively rename the keys in an object.
+ */
+export function renameKeys<T extends Dict>(object: T, fn: RenameKeysFn) {
   return renamer(object, fn);
 }
 
+/**
+ * Get object value based on string path
+ */
 export function get<T extends Dict>(object: T, path: string, fallback?: any) {
   const paths = resolvePath(path).join(".");
   return getter(object, paths, { default: fallback });
 }
 
+/**
+ * Set object value based on string path
+ */
 export function set<T extends any>(object: T, path: string, value: any): any {
   const paths = resolvePath(path).join(".");
   return setter(object, paths, value);
 }
 
+/**
+ * Delete object value based on string path
+ */
 export function remove(object: object, path: string) {
   const paths = resolvePath(path).join(".");
   return deleter(object, paths);
 }
 
+/**
+ * Check if an object has specific key based on path
+ */
 export function has(object: Dict, path: string) {
   const paths = resolvePath(path).join(".");
   return hasOwn(object, paths);
 }
 
-export type MapInterator<T> = (value: any, key: string, object: T) => void;
+export type MapFn<T> = (value: any, key: string, object: T) => void;
 
-export function map<T extends Dict>(object: T, fn: MapInterator<T>) {
+/**
+ * A function that maps over each key-value pair in the object.
+ *
+ * It calls the defined function on each item of an object,
+ * and returns an object that contains the results.
+ *
+ * @param object the object to map
+ * @param fn the callback to run
+ */
+export function map<T extends Dict>(object: T, fn: MapFn<T>) {
   const result: Dict = {};
 
   for (const key in object) {
@@ -46,21 +71,29 @@ export function map<T extends Dict>(object: T, fn: MapInterator<T>) {
   return result;
 }
 
-export type ForEachInterator<T> = (value: any, key: string, object: T) => void;
+export type ForEachFn<T> = (value: any, key: string, object: T) => void;
 
-export function forEach<T extends Dict>(object: T, fn: ForEachInterator<T>) {
+/**
+ * Performs the specified action for each element in an object.
+ *
+ * @param object the object to loop through
+ * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the object.
+ */
+export function forEach<T extends Dict>(object: T, fn: ForEachFn<T>) {
   for (const key in object) {
     fn(object[key], key, object);
   }
 }
 
-export type FilterInterator<T> = (
-  value: any,
-  key: string,
-  object: T
-) => boolean;
+export type FilterFn<T> = (value: any, key: string, object: T) => boolean;
 
-export function filter<T extends Dict>(object: T, fn: FilterInterator<T>) {
+/**
+ * Returns the items of an object that meet the condition specified in a callback function.
+ *
+ * @param object the object to loop through
+ * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the object.
+ */
+export function filter<T extends Dict>(object: T, fn: FilterFn<T>) {
   const result: Dict = {};
 
   for (const key in object) {
@@ -75,6 +108,12 @@ export function filter<T extends Dict>(object: T, fn: FilterInterator<T>) {
   return result;
 }
 
+/**
+ * Return a copy of an object without the given keys.
+ *
+ * @param object the object to loop through
+ * @param keys the keys to omit
+ */
 export function omit<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   const result: Dict = {};
 
@@ -88,6 +127,12 @@ export function omit<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   return result as Omit<T, K>;
 }
 
+/**
+ * Return a copy of an object with the given keys.
+ *
+ * @param object the object to loop through
+ * @param keys the keys to pick
+ */
 export function pick<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   const result = {} as { [P in K]: T[P] };
   for (const key of keys) {
@@ -98,6 +143,12 @@ export function pick<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   return result;
 }
 
+/**
+ * Returns an array of the picked and omited copy of an object based on keys.
+ *
+ * @param object the object to loop through
+ * @param keys the keys to pick
+ */
 export function split<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   const picked: Dict = {};
   const omitted: Dict = {};
@@ -113,12 +164,18 @@ export function split<T extends Dict, K extends keyof T>(object: T, keys: K[]) {
   return [picked, omitted] as [{ [P in K]: T[P] }, Omit<T, K>];
 }
 
+/**
+ * Returns the keys of an object with correct types
+ */
 export function keys<T extends Dict>(object: T) {
   return (Object.keys(object) as unknown) as (keyof T)[];
 }
 
+/**
+ * Returns the [key, value] pair of an object with correct types
+ */
 export function entries<T extends Dict>(object: T) {
   return Object.entries(object) as [keyof T, T[keyof T]][];
 }
 
-export { merge, isEqual };
+export { merge, isEqual, copy };
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,16 @@
 export type Dict = Record<string, any>;
 
+/**
+ * Returns `true` if the object is a "plain" object
+ */
 export function isObject(value: any): value is Dict {
   return value != null && typeof value === "object" && !Array.isArray(value);
 }
 
-export function isEmptyObject(value: any) {
+/**
+ * Returns `true` if the number of properties in the object is zero.
+ */
+export function isEmpty(value: any) {
   return isObject(value) && Object.keys(value).length === 0;
 }
 
@@ -12,6 +18,9 @@ export function isValidObject(value: any): value is Dict {
   return isObject(value) || Array.isArray(value) || typeof value === "function";
 }
 
+/**
+ * Converts path string to arrays
+ */
 export function resolvePath(path: string) {
   return path.replace(/\[/g, ".").replace(/\]/g, "").split(".");
 }
